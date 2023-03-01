213ca94da956d22825d6474d96316b53348ac311
Arash Hosseini <arashhosseini733@gmail.com>
2023-03-01 16:46:09 +0330

    change style

--- components/RotateCard.tsx
+++ components/RotateCard.tsx
@@ -14,7 +14,7 @@ function RotateCard({title,image,description}:Props) {
                 <div className="absolute inset-0 space-y-8 rounded-xl flex flex-col items-center justify-center ring-2 ring-[#FF821E]">
                     <Image src={image.src} width="300" height="300" alt='icon' objectFit="cover" className='h-[40%] w-[40%] min-[500px]:w-[50%] sm:w-[40%] lg:w-[50%] xl:w-[40%] object-cover' />
                     <p className='text-white text-lg'>{title}</p>
-                    <button className='text-white w-[70%] p-2 rounded-full sm:text-base border-2 border-b-0'>مشاهده توضیحات</button>
+                    <button className='text-white w-[70%] p-2 py-3 rounded-full sm:text-base border-2 border-b-0'>مشاهده توضیحات</button>
                 </div>
                 <div className='absolute inset-0 h-full w-full rounded-xl bg-[#FF821E]  text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]'>
                     <div className='flex min-h-full flex-col  justify-center space-y-8'>
