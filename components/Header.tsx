da66c516672da15960384595749cd2c7f1cba639
Arash Hosseini <arashhosseini733@gmail.com>
2023-03-12 02:50:09 +0330

    smoth scroll

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Logo from "../assets/images/logo.png";
-import HamberMenu from "./HamberMenu";
 import { useRecoilState } from "recoil";
 import { menuState } from "../atoms/menuAtom";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
@@ -17,13 +16,13 @@ function Header() {
           <div className="lg:flex lg:items-center lg:justify-between w-full max-w-4xl mx-auto">
             <div className="flex justify-between items-center">
               <div className="text-xl font-semibold text-gray-700">
-                  <Link href="#">
-                    <Image src={Logo.src} alt="" width="220" height="220" />
-                  </Link>
+                <Link href="#">
+                  <Image src={Logo.src} alt="" width="220" height="220" />
+                </Link>
               </div>
 
               <div className="flex lg:hidden">
-              {open ? (
+                {open ? (
                   <XMarkIcon
                     className="w-7 h-7 text-white opacity-80 hover:opacity-100"
                     onClick={() => setOpen(false)}
@@ -38,21 +37,32 @@ function Header() {
             </div>
 
             <div className="hidden -mx-4 lg:flex lg:items-center">
-              <Link href="#about">
-                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E] hover:text-[#FF821E]/80">معرفی پرسیس</button>
+                <Link href="#about" scroll={false}>
+                  <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E] hover:text-[#FF821E]/80">
+                    معرفی پرسیس
+                  </button>
+                </Link>
+
+              <Link href="#about" scroll={false}>
+                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E]  hover:text-[#FF821E]/80">
+                  معرفی بازخورد
+                </button>
               </Link>
-              <Link href="#about">
-                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E]  hover:text-[#FF821E]/80">معرفی بازخورد</button>
-             </Link>
-             <Link href="#why">
-                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E]  hover:text-[#FF821E]/80 border-b-[1px] border-[#FF821E] ">چرا رویداد هیئت مدیره؟</button>
-             </Link>
-             <Link href="#contact">
-                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E] hover:text-[#FF821E]/80">ثبت نام</button>
-             </Link>
-             <Link href="#contact">
-                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E] hover:text-[#FF821E]/80">تماس با ما</button>
-             </Link>
+              <Link href="#why" scroll={false}>
+                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E]  hover:text-[#FF821E]/80 border-b-[1px] border-[#FF821E] ">
+                  چرا رویداد هیئت مدیره؟
+                </button>
+              </Link>
+              <Link href="#contact" scroll={false}>
+                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E] hover:text-[#FF821E]/80">
+                  ثبت نام
+                </button>
+              </Link>
+              <Link href="#contact" scroll={false}>
+                <button className="block mx-4 mt-2 md:mt-0 text-sm text-[#FF821E] hover:text-[#FF821E]/80">
+                  تماس با ما
+                </button>
+              </Link>
             </div>
           </div>
         </div>
@@ -65,7 +75,12 @@ function Header() {
               بازخورد - بازوی آموزشی گروه پرسیس
             </h1>
             <h2 className="text-[#FF821E] text-sm sm:text-base md:text-xl">
-              <span className="border-b-[1px] border-[#FF821E]">نگاهی</span> کاربردی به آموزش <span className="border-b-[1px] border-[#FF821E] -mr-1">اتکا به</span> تجربه و امید به آینده
+              <span className="border-b-[1px] border-[#FF821E]">نگاهی</span>{" "}
+              کاربردی به آموزش{" "}
+              <span className="border-b-[1px] border-[#FF821E] -mr-1">
+                اتکا به
+              </span>{" "}
+              تجربه و امید به آینده
             </h2>
           </div>
         </div>
