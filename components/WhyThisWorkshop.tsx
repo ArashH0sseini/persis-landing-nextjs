54183a9cfd06aeab87436d74dc9c7400a26d595e
Arash Hosseini <arashhosseini733@gmail.com>
2023-03-12 03:28:21 +0330

    add animation

--- components/WhyThisWorkshop.tsx
+++ components/WhyThisWorkshop.tsx
@@ -13,7 +13,7 @@ function WhyThisWorkshop() {
             چرا رویداد هیئت مدیره
           </h3>
         </div>
-        <div className="flex border-b-2 border-t-2 border-[#FF821E] items-center rounded-[2.5rem] p-6 mt-6 ">
+        <div className="flex border-b-2 border-t-2 border-[#FF821E] items-center rounded-[2.5rem] p-6 mt-6">
           <p className="text-justify text-xl leading-10 ">
             شرکت خصوصی مشاوره پرسیس در سال ۱۴۰۰ توسط تعدادی از فعالین حوزه
             مشاوره و سرمایه‌گذاری تاسیس شده است. این شرکت در زمينه درک بهتر
@@ -32,16 +32,16 @@ function WhyThisWorkshop() {
           <img
             src={quote.src}
             alt=""
-            className="w-14 lg:w-20 rotate-180 mr-10"
+            className="w-14 lg:w-20 rotate-180 mr-10" style={{animation: 'pingg2 2s cubic-bezier(0, 0, 0.2, 1) infinite'}}
           />
-          <p className="text-justify text-base lg:text-lg leading-10 px-10 border-r-2 border-l-2 border-[#FF821E]">
+          <p className="text-justify text-base lg:text-lg leading-10 px-10 border-r-2 border-l-2 border-[#FF821E] mt-4">
             رید هافمن (موسس لینکدین) در جایی از پادکست Masters of Scale می‌گوید:
             «هیئت مدیره می‌تواند شرکت شما را موفق کند یا آن را از بین ببرد اما
             در واقعیت شانس این‌که باعث غرق شدن کشتی کسب‌وکار شود خیلی بیشتر از
             هیئت مدیره‌ای معجزه‌آسا است که بتواند شما را نجات دهد.»
           </p>
           <div className="flex justify-end ml-10">
-            <img src={quote.src} alt="" className="w-14 lg:w-20" />
+            <img src={quote.src} alt="" className="w-14 lg:w-20" style={{animation: 'pingg 2s cubic-bezier(0, 0, 0.2, 1) infinite'}} />
           </div>
         </div>
       </div>
